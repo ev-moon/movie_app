4a4767ece860a959f4cb077b7cfaf6e880155c47
ev-moon <xenos96111@gmail.com>
2020-10-09 19:36:14 +0900

    Updates

---
+++ precache-manifest.b568769b2b0142d9a4ec3008536a929c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b48d46621ca88b8c9c899e38907f0d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b48ccb66003c739407cb",
+    "url": "/movie_app/static/css/main.c6282052.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48ccb66003c739407cb",
+    "url": "/movie_app/static/js/main.81e0cc15.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
