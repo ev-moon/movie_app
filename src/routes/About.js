47333712fbc906775275366eaba425ce74ebcdc2
ev-moon <xenos96111@gmail.com>
2020-10-10 15:00:15 +0900

    feat: routes and navigation

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>Practice React App using Movie API</span>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+    console.log(location.state);
+  }
+  render() {
+    const { location } = this.props; // will cause error if location is undefined--render() executes earlier than componentDidMount
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
+}
+export default Detail;
